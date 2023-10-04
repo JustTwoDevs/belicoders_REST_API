36dd26aa58bde3b0d922943e31f47188edce5225
carlos paez <carlosa.paezc@autonoma.edu.co>
2023-10-03 19:49:06 -0500

    end

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -7,11 +7,13 @@ const authMiddleware = (req, res, next) => {
     if (!token)
       return res
         .status(401)
-        .json({ message: "No token, authorization denied" });
+        .json({ message: "No authorization token, authorization denied" });
 
     jwt.verify(token, process.env.TOKEN_SECRET, (error, user) => {
       if (error) {
-        return res.status(401).json({ message: "Token is not valid" });
+        return res
+          .status(401)
+          .json({ message: "Authorization token is not valid" });
       }
       req.user = user;
       next();
