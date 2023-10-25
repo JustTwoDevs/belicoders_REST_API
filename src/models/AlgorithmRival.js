6d35c0af3a62c2a49041f25095153c048b714ed8
carlos paez <carlosa.paezc@autonoma.edu.co>
2023-10-24 23:54:49 -0500

    i forgot to change something

--- src/models/AlgorithmRival.js
+++ src/models/AlgorithmRival.js
@@ -2,9 +2,7 @@ import Rival from "./Rival.js";
 import models, { Schema } from "mongoose";
 
 const algorithmRivalSchema = new Schema({
-  solutionCode: { type: String },
   inputCases: { type: String },
-  expectedOutput: { type: String },
 });
 
 export default models.AlgorithmRival |
